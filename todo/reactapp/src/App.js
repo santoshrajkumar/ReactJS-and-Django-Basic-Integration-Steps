3c62c2c2dc612668ceb05c6db3e785f435f4776e
IOCL Dimapur <iocldimapur@gmail.com>
2020-07-27 22:50:58 +0530

    first

---
+++ todo/reactapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <h4>This your first React App connected to Django</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
